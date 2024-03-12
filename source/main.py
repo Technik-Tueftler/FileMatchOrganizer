d5b786ab0f85b1464633b5d7f84303c12aa37ea8
Technik Tüftler <technik.tueftler@gmail.com>
2024-03-12 15:55:13 +0100

    filter all the pdfs and search for the pattern

--- source/main.py
+++ source/main.py
@@ -1,17 +1,65 @@
 """Doc
 """
+
 import os
+import platform
+from pathlib import Path
+import re
+from dataclasses import dataclass, field
+from typing import List
 import tkinter as tk
 from tkinter import PhotoImage, filedialog
+from tempfile import TemporaryDirectory
+from PIL import Image
+from pdfquery import PDFQuery
+from pdf2image import convert_from_path
+import pytesseract
 
 from constants import APP_INFORMATION, INITIAL_DIRECTORY
 
+pattern = re.compile(r'\b\d+\s[A-Z]+\s\d+/\d+\b')
 
+if platform.system() == "Windows":
+    # We may need to do some additional downloading and setup...
+    # Windows needs a PyTesseract Download
+    # https://github.com/UB-Mannheim/tesseract/wiki/Downloading-Tesseract-OCR-Engine
+
+    pytesseract.pytesseract.tesseract_cmd = (
+        r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    )
+
+    # Windows also needs poppler_exe
+    # https://github.com/oschwartz10612/poppler-windows/releases/
+
+    path_to_poppler_exe = Path(
+        r"C:\Users\Technik_Tueftler\Downloads\Release-24.02.0-0\poppler-24.02.0\Library\bin"
+    )
+
+    # Put our output files in a sane place...
+    out_directory = Path(r"~\Desktop").expanduser()
+else:
+    out_directory = Path("~").expanduser()
+
+@dataclass
+class AnalyzedFile:
+    """Class of a analyzed pdf file for further processing
+    """
+    start_file_path: Path
+    target_file_path: Path
+    matched_pattern: List[int] = field(default_factory=list)
+    analyzed_pages: List[int] = field(default_factory=list)
+    def __repr__(self):
+        if self.start_file_path is not None:
+            return f"AnalyzedFile: {self.start_file_path}"
+        return f"AnalyzedFile: {self.target_file_path}"
+
+@dataclass
 class AppSettings:
-    def __init__(self):
-        self.initial_dir = INITIAL_DIRECTORY
-        self.folder_start_path = None
-        self.folder_target_path = None
+    """Generic class to organize settings 
+    """
+    initial_dir = INITIAL_DIRECTORY
+    folder_start_path = None
+    folder_target_path = None
 
 
 settings = AppSettings()
@@ -23,9 +71,7 @@ class FileOrganizerApp:
     def __init__(self, root):
         self.root = root
         self.root.title("File Match Organizer")
-        # self.root.geometry("500x300")
-        icon = PhotoImage(file="../files/icon.png")
-        root.iconphoto(True, icon)
+        root.iconphoto(True, PhotoImage(file="../files/icon.png"))
 
         # Menü erstellen
         self.menu_bar = tk.Menu(root)
@@ -59,39 +105,71 @@ class FileOrganizerApp:
         self.finish_button = tk.Button(root, text="Beenden", command=self.finish_app)
         self.finish_button.pack(pady=10)
 
+        # Load the image
+        # image_2 = Image.open("89orp8.jpg")
+        # photo_2 = ImageTk.PhotoImage(image_2)
+
+        # Label widget to display the image
+        # self.labelimage = tk.Label(root, image=photo_2)
+        # self.labelimage.image = photo_2
+        # self.labelimage.pack(pady=10)
+
+    # def update_picture(self, image):
+    # self.labelimage.configure(image=image)
+    # self.labelimage.image = image
+
     def choose_start_folder(self):
         folder_start_path = filedialog.askdirectory(
             title="Ordner auswählen", initialdir=settings.initial_dir
         )
         settings.folder_start_path = folder_start_path
         # Hier könntest du den ausgewählten Ordnerpfad weiterverarbeiten oder speichern
+        # image_3 = Image.open("89orp82.jpg")
+        # photo_3 = ImageTk.PhotoImage(image_3)
+
+        # Label widget to display the image
+        # self.labelimage.configure(image=photo_3)
+        # self.labelimage.image = photo_3
 
     def choose_target_folder(self):
         target_folder_path = filedialog.askdirectory(
             title="Ordner auswählen", initialdir=settings.initial_dir
         )
         settings.folder_target_path = target_folder_path
-        # Hier könntest du die ausgewählte Datei weiterverarbeiten oder speichern
 
     def organize_files(self):
-        # Hier könntest du die Logik implementieren, um die Dateien zu organisieren
-        print(settings.folder_start_path)
-        print(settings.folder_target_path)
-        print(25*"-")
-        temp_files = []
-        for root, dirs, files in os.walk(settings.folder_start_path):
+        """Handle function to analyze files and reorder
+        """
+        start_files = []
+        end_files = []
+        for root_path, _, files in os.walk(settings.folder_start_path):
             for name in files:
-                if name.endswith(".txt"):
-                    temp_files.append(name)
-            print(f"root: {root}")
-            print(f"dirs: {dirs}")
-            print(f"files: {files}")
-            print(25*"-")
-        print(f"names: {temp_files}")
-        
-        for root, dirs, files in os.walk(settings.folder_target_path):
+                if name.endswith(".pdf"):
+                    with TemporaryDirectory() as tempdir:
+                        pdf_file = Path(os.path.join(root_path, name))
+                        images = converting_pdf_to_images(tempdir, pdf_file)
+                        text = recognizing_text(images)
+                        all_matches = [re.findall(pattern, s) for s in text]
+                        print(all_matches)
+                        start_files.append(AnalyzedFile(start_file_path=pdf_file,
+                                                        target_file_path=None,
+                                                        matched_pattern=all_matches,
+                                                        analyzed_pages=text))
+                        print(25*"#")
+
+        for root_path, _, files in os.walk(settings.folder_target_path):
             for name in files:
-                ...
+                if name.endswith(".pdf"):
+                    with TemporaryDirectory() as tempdir:
+                        pdf_file = Path(os.path.join(root_path, name))
+                        images = converting_pdf_to_images(tempdir, pdf_file)
+                        text = recognizing_text(images)
+                        all_matches = [re.findall(pattern, s) for s in text]
+                        print(all_matches)
+                        end_files.append(AnalyzedFile(start_file_path=None,
+                                                        target_file_path=pdf_file,
+                                                        matched_pattern=all_matches,
+                                                        analyzed_pages=text))
 
     def finish_app(self):
         self.root.destroy()
@@ -111,6 +189,58 @@ class FileOrganizerApp:
     def show_settings(self):
         # Hier könntest du ein Fenster oder Popup für Einstellungen anzeigen
         pass
+
+
+def converting_pdf_to_images(tempdir: str, pdf_path: Path) -> List[str]:
+    """Function read all PDF pages anc converting it to pictures
+
+    Args:
+        pdf_path (str): Path of the PDF file
+
+    Returns:
+        list[str]: List of paths with pictures
+    """
+    image_file_list = []
+    if platform.system() == "Windows":
+        pdf_pages = convert_from_path(pdf_path, 500, poppler_path=path_to_poppler_exe)
+    else:
+        pdf_pages = convert_from_path(pdf_path, 500)
+    for page_enumeration, page in enumerate(pdf_pages, start=1):
+        print(tempdir)
+        filename = f"{tempdir}\page_{page_enumeration:03}.jpg"
+        page.save(filename, "JPEG")
+        image_file_list.append(filename)
+    return image_file_list
+
+
+def recognizing_text(image_file_list: List[str]) -> List[str]:
+    """Function recognizes text from image
+
+    Args:
+        image_file_list (list[str]): List of paths from images
+
+    Returns:
+        list[str]: List of recognized text from images
+    """
+    text_file_list = []
+    for image_file in image_file_list:
+        text = str(((pytesseract.image_to_string(Image.open(image_file), lang="deu"))))
+        text = text.replace("-\n", "").replace("\n", " ")
+        text_file_list.append(text)
+    return text_file_list
+
+
+def extract_text_from_pdf(root_path: str, name: str) -> str:
+    """Extract text from PDF
+    Args:
+        root_path (str): Completely path of the file
+        name (str): Name of the PDF file
+    """
+    pdf = PDFQuery(os.path.join(root_path, name))
+    pdf.load()
+    text_elements = pdf.pq("LTTextLineHorizontal")
+    text = [t.text for t in text_elements]
+    return text
 
 
 if __name__ == "__main__":
